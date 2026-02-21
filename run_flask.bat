423a2289a6bed5078e29e72022f0ea94a25f0f12
Ralph Matthew A. Samonte <ralphmatthew.samonte@gmail.com>
2026-02-21 22:04:47 +0800

    Fixed things

--- run_flask.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Flask...
-pause
-
-:: Use Python from your virtual environment directly
-C:\Users\Mateo\venv\Scripts\python.exe C:\xampp\htdocs\PesoSnartHire\app.py
-
-pause
